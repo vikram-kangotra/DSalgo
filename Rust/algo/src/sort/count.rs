1eef050f3a565cd158be4e80bd446db3f65c8e3c
Vikram-Kangotra <vikramkangotra8055@gmail.com>
2022-10-03 02:58:06 +0530

    used a better way to create vector of pre-defined length

--- Rust/algo/src/sort/count.rs
+++ Rust/algo/src/sort/count.rs
@@ -1,8 +1,7 @@
 pub fn counting_sort(arr: &mut [usize]) {
     let max = arr.iter().max().unwrap();
 
-    let mut temp = Vec::new();
-    temp.resize(max + 1, 0);
+    let mut temp = vec![0; max + 1];
 
     for val in arr.iter() {
         temp[*val] += 1;
