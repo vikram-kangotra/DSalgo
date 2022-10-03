466640189e9f7916259ee0e0c2838ffe86eb4738
A.R.(++X)AN <59042198+aryan-bansal@users.noreply.github.com>
2022-10-03 21:21:43 +0530

    Add files via upload

---
+++ C/ALGORITHMS/SEARCH/binary.c
@@ -0,0 +1,21 @@
+int binary_search(int arr[], int n, int x)
+{
+    //'x' is the element that is to be searched
+    int low = 0, mid, high;
+    high = n-1;
+    while(low <= high){
+        mid = low + (high-low)/2;
+        if(arr[mid]==x){
+            return mid;
+            break;
+        }
+        else if(arr[mid]>x){
+            high = mid-1;
+
+        }
+        else{
+            low = mid+1;
+        }
+    }
+    return -1;
+}
