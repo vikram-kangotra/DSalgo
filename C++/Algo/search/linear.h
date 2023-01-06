b1c1a73b46efb43853a9b954b24901d806c25ccc
Vikram-kangotra <vikramkangotra8055@gmail.com>
2023-01-06 22:22:45 +0530

    narrowed down the linear search implementation

--- C++/Algo/search/linear.h
+++ C++/Algo/search/linear.h
@@ -1,27 +1,12 @@
-#include<iostream>
-using namespace std;
-void linearSearch(int *arr, int size, int x){
-    int *a = new int[size];
-    int pos=0;
+#include <iostream>
+#include <vector>
+
+std::vector<int> linear_search(int* arr, size_t size, int x){
+    std::vector<int> result;
     for(int i=0; i<size; i++){
         if(arr[i] == x){
-            a[pos] = i;
-            pos++;
+            result.push_back(i);
         }
     }
-    if(pos==0){
-        cout<<"Number not present";
-    }
-    else{
-        cout<<"Number present at location(s):"<<endl;
-        cout<<a[0]<<endl;
-        for(int i=1; i<sizeof(a); i++){
-            if(a[i-1]<a[i]){
-                cout<<a[i]<<endl;
-            }
-            else{
-                break;
-            }
-        }
-    }
+    return result;
 }
