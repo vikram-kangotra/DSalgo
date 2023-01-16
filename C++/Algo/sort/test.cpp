f32b6588eeeb2910f6fcfc7091d832d2de3e7731
Disha-Baghel <bagheldisha708@gmail.com>
2023-01-16 21:24:10 +0530

    Added new file

--- C++/Algo/sort/test.cpp
+++ C++/Algo/sort/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "bubbleSort.h"
 #include "insertionSort.h"
+#include "selectionSort.h"
 #include <cassert>
 
 void test_bubble_sort() {
@@ -17,10 +18,18 @@ void test_insertion_sort() {
     assert(vec == sorted && "insertion sort failed");
 }
 
+void test_selection_sort(){
+    auto vec = std::vector<int>{6, 5, 3, 2, 8, 1, 7, 4};
+    auto sorted = std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8};
+    selectionSort(vec);
+    assert(vec == sorted && "selection sort failed");
+}
+
 int main() {
 
     test_bubble_sort();
     test_insertion_sort();
+    test_selection_sort();
 
     return 0;
 }
