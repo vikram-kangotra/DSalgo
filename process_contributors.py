658dd8ea6d6f3b1f7388212bfde73f2f7ff102f8
Vikram-kangotra <vikramkangotra8055@gmail.com>
2023-01-31 04:54:29 +0530

    Added image resize

--- process_contributors.py
+++ process_contributors.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as f:
 for i, line in enumerate(readme):
     if line.startswith('## Top 3 Contributors'):
         for j, contributor in enumerate(contributors[:3]):
-            readme[i+j+1] = f"- ![alt text]({contributor['avatar_url']} | width=24px)[{contributor['login']}]({contributor['html_url']}) ({contributor['contributions']})\n"
+            readme[i+j+1] = "- ![alt text]({}|width=24px)[{}]({}) ({})\n".format(contributor['avatar_url'], contributor['login'], contributor['html_url'], contributor['contributions'])
 
 
 with open("README.md", "w") as f:
