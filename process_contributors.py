1ba09ccce00364f4b1676f3ea985e82a702c0a41
Vikram-kangotra <vikramkangotra8055@gmail.com>
2023-01-31 01:00:05 +0530

    Added python srcipt to update README.md with top 3 contributors

---
+++ process_contributors.py
@@ -0,0 +1,20 @@
+import json
+
+with open('contributors.json', 'r') as f:
+    contributors = json.load(f)
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+new_readme = ""
+
+for line in readme.splitlines():
+    if line.startswith('## Top 3 Contributors'):
+        new_readme += line + "\n"
+        for contributor in contributors[:3]:
+            new_readme += f"- [{contributor['login']}]({contributor['html_url']}) ({contributor['contributions']})" + "\n"
+
+    else:
+        new_readme += line + "\n"
+
+print(new_readme)
