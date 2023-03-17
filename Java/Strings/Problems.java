782662755dcae4103ccad8465bda336efcbc5116
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-03-17 22:08:41 +0530

    reverseWords

--- Java/Strings/Problems.java
+++ Java/Strings/Problems.java
@@ -72,6 +72,28 @@ public class Problems{
         }
         return -1;
     }
+
+    public void reverseWords(char[] s){
+        reverse(s, 0, s.length - 1);
+        int start = 0;
+        for(int i = 0; i < s.length; i++){
+            if(s[i] == ' '){
+                reverse(s, start, i - 1);
+                start = i + 1;
+            }
+        }
+        reverse(s, start, s.length - 1);
+    }
+
+    private void reverse(char[] s, int start, int end){
+        while(start < end){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
     public static void main(String[] args){
         
     }
