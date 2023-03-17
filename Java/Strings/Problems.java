63861becf50f3df73a8204078f35ba89be023ea0
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-03-17 21:55:19 +0530

    indexOfLeftmostNonRepeatingCharacter

--- Java/Strings/Problems.java
+++ Java/Strings/Problems.java
@@ -59,8 +59,19 @@ public class Problems{
         }
         return res;
     }
-    
 
+    public int indexOfLeftmostNonRepeatingCharacter(String s){
+        int[] count = new int[256];
+        for(int i = 0; i < s.length(); i++){
+            count[s.charAt(i)]++;
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(count[s.charAt(i)] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args){
         
     }
