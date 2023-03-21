3e3b61b77948aadadbb9b5b0629bfe6d9237e2e0
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-03-21 09:36:00 +0530

    Rabin-Karp Algo

--- Java/Strings/PatternSearch.java
+++ Java/Strings/PatternSearch.java
@@ -1,3 +1,69 @@
 public class PatternSearch {
     
+    // Rabin-Karp algorithm
+    /*
+     * 1. Compute the hash value of the pattern
+     * 2. Compute the hash value of the first window of text
+     * 3. Compare the hash value of the pattern and the first window of text
+     * 4. If the hash value of the pattern and the first window of text are the same, compare the pattern and the first window of text
+     * 5. If the hash value of the pattern and the first window of text are not the same, compute the hash value of the next window of text
+     * 6. Repeat step 3 to 5 until the end of the text
+     */
+    
+    public static final int d = 256;
+    /* pat -> pattern
+        txt -> text
+        q -> A prime number
+    */
+
+    public void search1(String pat, String txt, int q){
+        int M = pat.length();
+        int N = txt.length();
+        int i, j;
+        int p = 0; // hash value for pattern
+        int t = 0; // hash value for txt
+        int h = 1;
+ 
+        // The value of h would be "pow(d, M-1)%q"
+        for (i = 0; i < M - 1; i++)
+            h = (h * d) % q;
+
+        // Calculate the hash value of pattern and first window of text
+        for (i = 0; i < M; i++){
+            p = (d * p + pat.charAt(i)) % q;
+            t = (d * t + txt.charAt(i)) % q;
+        }
+
+        // Slide the pattern over text one by one
+        for (i = 0; i <= N - M; i++){
+            // Check the hash values of current window of text and pattern. If the hash values match then only check for characters on by one
+            if ( p == t ){
+                /* Check for characters one by one */
+                for (j = 0; j < M; j++){
+                    if (txt.charAt(i + j) != pat.charAt(j))
+                        break;
+                }
+                // if p == t and pat[0...M-1] = txt[i, i + 1, ...i + M-1]
+                if (j == M)
+                    System.out.println("Pattern found at index " + i);
+            }
+            // Calculate hash value for next window of text: Remove leading digit, add trailing digit
+            if ( i < N - M ){
+                t = (d * (t - txt.charAt(i) * h) + txt.charAt(i + M)) % q;
+                // We might get negative value of t, converting it to positive
+                if (t < 0)
+                    t = (t + q);
+            }
+        }
+    }
+
+    
+
+    public static void main(String[] args){
+        String txt = "GEEKS FOR GEEKS";
+        String pat = "GEEK";
+        int q = 101; // A prime number
+        PatternSearch ps = new PatternSearch();
+        ps.search1(pat, txt, q);
+    }
 }
