11737f94f58a71d2c0d76dcea54e849ba8ca1737
Itz-Verma <princekverma0702@gmail.com>
2023-04-14 12:38:36 +0530

    modified push.c

--- C/DS/stack/linkedListImplementation/push.c
+++ C/DS/stack/linkedListImplementation/push.c
@@ -1,7 +1,12 @@
 #include "Stack.h"
+#include <stdio.h>
 
 void push(Node** top, int data) {
     Node* newNode = malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Stack Overflow!\n");
+        return;
+    }
     newNode->data = data;
     newNode->next = *top;
     *top = newNode;
