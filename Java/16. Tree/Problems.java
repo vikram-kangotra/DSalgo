260b122247b8cf12069496167ec50c1f04cfe512
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-05-18 00:16:07 +0530

    Added some important problems on Trees

--- Java/16. Tree/Problems.java
+++ Java/16. Tree/Problems.java
@@ -1,3 +1,5 @@
+
+import java.util.*;
 public class Problems {
     // Height of Binary Tree
 
@@ -53,5 +55,145 @@ public class Problems {
         return Math.max(root.getData(), Math.max(maximum(root.getLeft()), maximum(root.getRight())));
     }
 
-    // 
+    // Print left view of Binary Tree
+
+    public void printLeftIteratively(TreeNode root){
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            for (int i = 1; i <= n; i++) {
+                TreeNode current = queue.remove();
+                if (i == 1) {
+                    System.out.print(current.getData() + " ");
+                }
+                if (current.getLeft() != null) {
+                    queue.add(current.getLeft());
+                }
+                if (current.getRight() != null) {
+                    queue.add(current.getRight());
+                }
+            }
+        }
+    }
+
+    // Print right view of Binary Tree
+
+    public void printRightIteratively(TreeNode root){
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            for (int i = 1; i <= n; i++) {
+                TreeNode current = queue.remove();
+                if (i == n) {
+                    System.out.print(current.getData() + " ");
+                }
+                if (current.getLeft() != null) {
+                    queue.add(current.getLeft());
+                }
+                if (current.getRight() != null) {
+                    queue.add(current.getRight());
+                }
+            }
+        }
+    }
+
+    // Children Sum Property
+
+    public boolean childrenSumProperty(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (root.getLeft() == null && root.getRight() == null) {
+            return true;
+        }
+        int sum = 0;
+        if (root.getLeft() != null) {
+            sum += root.getLeft().getData();
+        }
+        if (root.getRight() != null) {
+            sum += root.getRight().getData();
+        }
+        return (root.getData() == sum && childrenSumProperty(root.getLeft()) && childrenSumProperty(root.getRight()));
+    }
+    
+    // Check for Balanced Tree : A tree is balanced if the difference between the heights of left subtree and right subtree is not more than 1 for all nodes of the tree.
+
+    public boolean isBalanced1(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int leftHeight = height(root.getLeft());
+        int rightHeight = height(root.getRight());
+        return (Math.abs(leftHeight - rightHeight) <= 1 && isBalanced1(root.getLeft()) && isBalanced1(root.getRight()));
+    } /*This will take O(n^2) time */
+
+    public int isBalanced2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = isBalanced2(root.getLeft());
+        if (leftHeight == -1) {
+            return -1;
+        }
+        int rightHeight = isBalanced2(root.getRight());
+        if (rightHeight == -1) {
+            return -1;
+        }
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return 1 + Math.max(leftHeight, rightHeight);
+    } /*This will take O(n) time */
+
+    // Check for Symmetric Tree
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.getLeft(), root.getRight());
+    }
+
+    public boolean isSymmetric(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if ((root1 == null && root2 != null) || (root1 != null && root2 == null) || (root1.getData() != root2.getData())) {
+            return false;
+        }
+        return isSymmetric(root1.getLeft(), root2.getRight()) && isSymmetric(root1.getRight(), root2.getLeft());
+    }
+
+    // Maximum width of Binary Tree
+
+    public int maxWidth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int max = 0;
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            max = Math.max(max, n);
+            for (int i = 1; i <= n; i++) {
+                TreeNode current = queue.remove();
+                if (current.getLeft() != null) {
+                    queue.add(current.getLeft());
+                }
+                if (current.getRight() != null) {
+                    queue.add(current.getRight());
+                }
+            }
+        }
+        return max;
+    }
 }
