517e571cea397aeef58464847e73619961c83ac3
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-05-20 09:55:38 +0530

    Spiral level order traversal

--- Java/16. Tree/TreeTraversals.java
+++ Java/16. Tree/TreeTraversals.java
@@ -134,5 +134,115 @@ public class TreeTraversals {
         }
     }
 
+    public void levelOrderTraversalLineByLine1(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+        while (queue.size() > 1) {
+            TreeNode current = queue.remove();
+            if (current == null) {
+                System.out.println();
+                queue.add(null);
+                continue;
+            }
+            System.out.print(current.getData() + " ");
+            if (current.getLeft() != null) {
+                queue.add(current.getLeft());
+            }
+            if (current.getRight() != null) {
+                queue.add(current.getRight());
+            }
+        }
+    }
+
+    public void levelOrderTraversalLineByLine2(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            for (int i = 1; i <= n; i++) {
+                TreeNode current = queue.remove();
+                System.out.print(current.getData() + " ");
+                if (current.getLeft() != null) {
+                    queue.add(current.getLeft());
+                }
+                if (current.getRight() != null) {
+                    queue.add(current.getRight());
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void spiralLevelOrderTraversal1(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack1.push(root);
+        while (!stack1.isEmpty() || !stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                TreeNode current = stack1.pop();
+                System.out.print(current.getData() + " ");
+                if (current.getRight() != null) {
+                    stack2.push(current.getRight());
+                }
+                if (current.getLeft() != null) {
+                    stack2.push(current.getLeft());
+                }
+            }
+            while (!stack2.isEmpty()) {
+                TreeNode current = stack2.pop();
+                System.out.print(current.getData() + " ");
+                if (current.getLeft() != null) {
+                    stack1.push(current.getLeft());
+                }
+                if (current.getRight() != null) {
+                    stack1.push(current.getRight());
+                }
+            }
+        }
+    }
+
+    public void spiralLevelOrderTraversal2(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        boolean reverse = false;
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            Stack<TreeNode> stack = new Stack<>();
+            for (int i = 1; i <= n; i++) {
+                TreeNode current = queue.remove();
+                if (reverse) {
+                    stack.push(current);
+                } else {
+                    System.out.print(current.getData() + " ");
+                }
+                if (current.getLeft() != null) {
+                    queue.add(current.getLeft());
+                }
+                if (current.getRight() != null) {
+                    queue.add(current.getRight());
+                }
+            }
+            if (reverse) {
+                while (!stack.isEmpty()) {
+                    System.out.print(stack.pop().getData() + " ");
+                }
+            }
+            reverse = !reverse;
+        }
+    }
+
 }
 
