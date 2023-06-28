cfe43024cf694c6b1fcbdd4e4330200f17ae0c9f
Itz-Verma <princekverma0702@gmail.com>
2023-06-28 10:14:36 +0530

    add createNode function to increase code readabilty and ease.

---
+++ C/DS/linkedList/singlyLinkedList/createNode.c
@@ -0,0 +1,8 @@
+#include "linkedList.h"
+
+struct Node* createNode(int data) {
+    struct Node* newNode = malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
